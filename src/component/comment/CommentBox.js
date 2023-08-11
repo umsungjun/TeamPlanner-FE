0a3c2cea44f11363417aed1765a96a59600863cb
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-08-12 00:20:35 +0900

    :sparkles: chatUpdate

--- src/component/comment/CommentBox.js
+++ src/component/comment/CommentBox.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { Button, createTheme, IconButton, ThemeProvider ,Avatar } from "@mui/material";
 import Box from "@mui/material/Box";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import TextInput from "./TextInput";
+import TextField from "@mui/material/TextField";
 import theme from "../../style/theme";
+import TextInput from "./TextInput";
+import { API } from "../../api/api";
+import { useParams } from "react-router-dom";
+import SolidBtn from "../button/SolideBtn";
 
 export default function CommentBox({ commentData, changeFlag, flag }) {
   const theme = createTheme({
@@ -17,16 +20,78 @@ export default function CommentBox({ commentData, changeFlag, flag }) {
       },
     },
   });
-  const [open, setOpen] = React.useState(false);
 
+  const muiTheme = createTheme({
+    typography: {
+      fontFamily: "Pretendard",
+    },
+    palette: {
+      primary: {
+        main: "#FF7300",
+      },
+    },
+  });
+  
+  const [editedContent, setEditedContent] = useState(commentData.content);
+  const [currentContent, setContent] = useState(commentData.content);
+  const [open, setOpen] = React.useState(false);
+  const [editMode, setEditMode] = React.useState(false); // Add this state
+  const { boardId } = useParams();
+  
   const handleClick = () => {
     setOpen(!open);
   };
+  // const handleEditClick = () => {
+  //   setEditMode(!editMode); // Activate edit mode
+  // };
+  const handleEditClick = () => {
+    setEditMode(!editMode); // 토글
+  };
+
+
+   // 로그인 중인 유저의 아이디를 가져옴
+   const userInfoJSON = localStorage.getItem("userInfo");
+   const userInfo = userInfoJSON ? JSON.parse(userInfoJSON) : null;
+ 
+  
+    /** 엔터 키 눌렀을 때의 함수입니다. */
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleClick();
+    }
+  };
+  const handleSuccessClick = () => {
+    setEditMode(!editMode); // 토글
+
+    API.put(`/api/v1/board/${boardId}/comment`, {
+      commentId: commentData.commentId,
+      boardId: boardId,
+      content: editedContent
+      }
+    )
+      .then((res) => {
+        commentData.content = res.data.content;
+        setContent(editedContent);
+        setEditMode(false);
+      })
+      .catch((error) => {
+        alert(error);
+        // Handle errors (optional)
+        // console.error("Error creating reply:", error);
+      });
+
+  };
+
+  useEffect(() => {
+  
+  }, [currentContent]);
+
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <CommentBoxWrap>
+          
           <ul className="comment">
             <li>
               <IconButton sx={{ p: 0 }}>
@@ -35,11 +100,36 @@ export default function CommentBox({ commentData, changeFlag, flag }) {
               </IconButton>
               <div className="comment-text">
                 <h3>{commentData.username}</h3>
-                <p>{commentData.content}</p>
+                {editMode ?  <ThemeProvider theme={muiTheme}><TextInputWrap>
+               <TextField
+                fullWidth
+                id="outlined-basic"
+                variant="outlined"
+                multiline
+                maxRows={3}
+                onKeyPress={handleKeyPress} 
+                onChange={(event) => setEditedContent(event.target.value)}
+                value={editedContent}
+              /> </TextInputWrap></ThemeProvider>: ( <p>{commentData.content}</p>)}
+               
                 <h4>{commentData.updatedAt}</h4>
               </div>
             </li>
+            <div>
+            {userInfo && userInfo.username && commentData.username === userInfo.username ? (
+              editMode ? (
+                <div>
+                <SolidBtn handle={handleSuccessClick} text="수정완료"/>
+                <SolidBtn handle={() => setEditMode(false)} text="취소"/>
+                </div>
+              ) : (
+                <AddBtn onClick={handleEditClick}>수정</AddBtn>
+              )
+            ) : null}
             <AddBtn onClick={handleClick}>답글</AddBtn>
+            </div>
+
+            
           </ul>
           {open ? (
             <div className="add-text">
@@ -57,6 +147,27 @@ export default function CommentBox({ commentData, changeFlag, flag }) {
     </>
   );
 }
+
+const EditBox = styled.div`
+  textarea {
+    width: 100%;
+    height: 100px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+  }
+
+  .edit-btns {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 8px;
+
+    button {
+      margin-left: 8px;
+    }
+  }
+`;
 
 const CommentBoxWrap = styled(Box)`
   padding-bottom: 1rem;
@@ -108,11 +219,38 @@ const CommentBoxWrap = styled(Box)`
 `;
 
 const AddBtn = styled(Button)`
-  font-size: 1.4rem !important;
+  font-size: 1.0rem !important;
   color: #3b3b3b;
   font-weight: 600;
   text-decoration: underline;
   & > div {
     padding: 0;
+  }
+`;
+
+const TextInputWrap = styled(Box)`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-top: 1rem;
+  button {
+    width: 15%;
+  }
+  @media ${() => theme.device.mobile} {
+    button {
+      margin-left: 1rem;
+    }
+  }
+`;
+
+const StyledTextField = styled(TextField)`
+  width: 100%;
+  .MuiInputBase-root {
+    padding: 1.5rem 2rem !important;
+    height: 8rem;
+  }
+  textarea {
+    font-size: 1.6rem;
+    color: #3b3b3b;
   }
 `;
